0cdbfedbb063e7eccc8f638a6b138f6060b9e030
Gustavo Vasconcelos <gluiz9445@gmail.com>
2025-04-27 19:14:05 -0300

    add app.get to root

--- server/src/services/server.ts
+++ server/src/services/server.ts
@@ -1,35 +1,39 @@
-import fastifyCors from '@fastify/cors'
-import fastify from 'fastify'
+import fastifyCors from "@fastify/cors";
+import fastify from "fastify";
 import {
-  serializerCompiler,
-  validatorCompiler,
-  type ZodTypeProvider,
-} from 'fastify-type-provider-zod'
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-import { createCompletionRoute } from './routes/create-completion'
-import { createGoalRoute } from './routes/create-goal'
-import { createPendingGoalsRoute } from './routes/get-pending-goals'
-import { getWeekSummaryRoute } from './routes/get-week-summary'
+import { createCompletionRoute } from "./routes/create-completion";
+import { createGoalRoute } from "./routes/create-goal";
+import { createPendingGoalsRoute } from "./routes/get-pending-goals";
+import { getWeekSummaryRoute } from "./routes/get-week-summary";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: '*',
-})
+	origin: "*",
+});
 
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.register(createGoalRoute)
-app.register(createCompletionRoute)
-app.register(createPendingGoalsRoute)
-app.register(getWeekSummaryRoute)
+app.register(createGoalRoute);
+app.register(createCompletionRoute);
+app.register(createPendingGoalsRoute);
+app.register(getWeekSummaryRoute);
 
 app
-  .listen({
-    port: 3333,
-    host: "0.0.0.0"
-  })
-  .then(() => {
-    console.log('HTTP Server running.')
-  })
+	.listen({
+		port: 3333,
+		host: "0.0.0.0",
+	})
+	.then(() => {
+		console.log("HTTP Server running.");
+	});
+
+app.get("/", async (request, reply) => {
+	return { message: "Welcome to in.Orbit API!" };
+});
